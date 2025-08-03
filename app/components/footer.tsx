713f8db9833105a15b9168a9b3e35e6655eff76f
avan <100318118+avansear@users.noreply.github.com>
2025-08-03 09:38:37 -0500

    arrow iupdate

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,9 +3,9 @@ import { HoverButton } from './hover-button'
 function ArrowIcon() {
   return (
     <svg
-      width="12"
-      height="12"
-      viewBox="0 0 12 12"
+      width="10"
+      height="10"
+      viewBox="0 0 10 10"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
