43133ad54577a178c2fb4636465226d9a8374b04
avan <100318118+avansear@users.noreply.github.com>
2025-08-04 16:46:48 -0500

    themes yay

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { InternalHoverButton } from './hover-button'
 
 const navItems = {
@@ -20,10 +22,10 @@ export function Navbar() {
     <aside className="-ml-[8px] mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className="flex flex-row items-center justify-between relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex flex-row space-x-0 pr-10">
+          <div className="flex flex-row space-x-0">
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
                 <InternalHoverButton key={path} href={path} className="m-1">
@@ -31,6 +33,24 @@ export function Navbar() {
                 </InternalHoverButton>
               )
             })}
+          </div>
+          <div className="flex flex-row space-x-2">
+            <button 
+              className="w-5 h-5 rounded-full bg-[#021F07] border-1 border-[#D75600] hover:border-[#D75600]/80 transition-colors"
+              onClick={() => {
+                document.documentElement.style.setProperty('--color-dark', '#021F07')
+                document.documentElement.style.setProperty('--color-light', '#D75600')
+                document.documentElement.style.setProperty('--color-accent', '#D75600BF')
+              }}
+            ></button>
+            <button 
+              className="w-5 h-5 rounded-full bg-[#1F0037] border-1 border-[#C60017] hover:border-[#C60017]/80 transition-colors"
+              onClick={() => {
+                document.documentElement.style.setProperty('--color-dark', '#1F0037')
+                document.documentElement.style.setProperty('--color-light', '#C60017')
+                document.documentElement.style.setProperty('--color-accent', '#C60017BF')
+              }}
+            ></button>
           </div>
         </nav>
       </div>
