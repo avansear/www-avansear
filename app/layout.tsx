e2556d00a211123516967a2d88c888eaf0ea1640
avan <100318118+avansear@users.noreply.github.com>
2025-08-04 18:56:08 -0500

    Revert "brain"
    This reverts commit 21e3542e7992eef52394257a3e267832cba17ccf.

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,7 +65,6 @@ export default function RootLayout({
       )}
     >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <div className="noise" />
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
