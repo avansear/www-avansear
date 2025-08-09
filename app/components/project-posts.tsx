14b984350bc70ce95754d42c53436e39572fc950
avan <100318118+avansear@users.noreply.github.com>
2025-08-08 19:55:23 -0500

    trunacted titlesin projects

--- app/components/project-posts.tsx
+++ app/components/project-posts.tsx
@@ -22,10 +22,10 @@ export function ProjectPosts() {
             href={`/projects/${post.slug}`}
           >
             <div className="w-full flex flex-row space-x-2">
-              <p className="text-[var(--color-light-80)] w-fit tabular-nums flex-shrink-0">
+              <p className="text-[var(--color-light-80)] w-fit tracking-tighter whitespace-nowrap flex-shrink-0">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
-              <p className="text-[var(--color-dark)] dark:text-[var(--color-light)] tracking-tight">
+              <p className="text-[var(--color-dark)] dark:text-[var(--color-light)] tracking-tighter truncate">
                 {post.metadata.title}
               </p>
             </div>
